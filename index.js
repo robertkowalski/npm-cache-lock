f8824b6441b081cd7b2d4f8304572998b8ce2164
Robert Kowalski <rok@kowalski.gd>
2014-02-28 00:40:11 +0100

    add code

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-module.exports = ""
+var crypto = require("crypto")
+  , getCacheStat = require("npm-cache-dir").getCacheStat
+  , path = require("path")
+  , lockFile = require("lockfile")
+
+
+var myLocks = exports.myLocks = {}
+
+exports.lockFileName = lockFileName
+function lockFileName (cache, log, u) {
+  var c = u.replace(/[^a-zA-Z0-9]+/g, "-").replace(/^-+|-+$/g, "")
+    , h = crypto.createHash("sha1").update(u).digest("hex")
+  h = h.substr(0, 8)
+  c = c.substr(-32)
+  log.silly("lockFile", h + "-" + c, u)
+  return path.resolve(cache, h + "-" + c + ".lock")
+}
+
+exports.lock = lock
+function lock (cache, log, u, opts, cb) {
+  // the cache dir needs to exist already for this.
+  getCacheStat(cache, log, function (er, cs) {
+    if (er) return cb(er)
+    var lf = lockFileName(cache, log, u)
+    log.verbose("lock", u, lf)
+    lockFile.lock(lf, opts, function (er) {
+      if (!er) myLocks[lf] = true
+      cb(er)
+    })
+  })
+}
+
+exports.unlock = unlock
+function unlock (cache, log, u, cb) {
+  var lf = lockFileName(cache, log, u)
+    , locked = myLocks[lf]
+
+  if (locked === false) {
+    return process.nextTick(cb)
+  } else if (locked === true) {
+    myLocks[lf] = false
+    lockFile.unlock(lockFileName(cache, log, u), cb)
+  } else {
+    throw new Error("Attempt to unlock " + u + ", which hasn't been locked")
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,76 @@
 var test = require("tap").test
+  , cacheLock = require("../")
+  , mkdirp = require("mkdirp")
+  , rimraf = require("rimraf")
+  , path = require("path")
+  , fs = require("fs")
 
-test("passing a string results in a string", function (t) {
-  t.equal(true, false)
+
+var opts = { stale: 1000
+           , retries: 3
+           , wait: 1000 }
+  , fixtures = path.join(__dirname, "fixtures")
+  , npm = { cache: path.join(fixtures, "mycache") }
+  , log = { silly: function () {}
+          , verbose: function () {} }
+
+function setup () {
+  rimraf.sync(fixtures)
+  mkdirp.sync(fixtures)
+}
+
+test("setup", function (t) {
+  setup()
+  t.end()
+})
+
+test("getting a lockfilename: uses the name of the file", function (t) {
+  t.has(cacheLock.lockFileName(npm.cache, log, "underscore@1.6.0")
+    , /-underscore-1-6-0.lock$/)
+  t.has(cacheLock.lockFileName(npm.cache, log
+    , "tar:///var/folders/mm/8hsp08pd3133564vw7n1yvf80000gn/T/" +
+      "npm-73731-CFYUYY1m/1393540354615-0.12562836962752044/tmp.tgz")
+  , /12562836962752044-tmp-tgz.lock$/)
+  t.end()
+})
+
+test("getting a lockfilename: uses the cachepath", function (t) {
+  t.has(cacheLock.lockFileName(npm.cache, log, "underscore@1.6.0")
+    , new RegExp("^" + npm.cache, 'g'))
+  t.end()
+})
+
+test("locking: creates a cachedir and a lockfile", function (t) {
+  cacheLock.lock(npm.cache, log, "underscore@1.6.0"
+    , opts, function (er) {
+    t.notOk(er)
+    t.ok(fs.existsSync(Object.keys(cacheLock.myLocks)[0]))
+    cacheLock.unlock(npm.cache, log, "underscore@1.6.0"
+    , function (er) {
+      t.notOk(er)
+      t.end()
+    })
+  })
+})
+
+test("unlocking: removes the lockfile", function (t) {
+  var lockFileName = cacheLock.lockFileName(npm.cache
+    , log, "test@1.7.0")
+
+  cacheLock.lock(npm.cache, log, "test@1.7.0"
+    , opts, function (er) {
+    t.notOk(er)
+    t.ok(fs.existsSync(lockFileName))
+    cacheLock.unlock(npm.cache, log, "test@1.7.0"
+    , function (er) {
+      t.notOk(er)
+      t.notOk(fs.existsSync(lockFileName), "lockfile does not exist")
+      t.end()
+    })
+  })
+})
+
+test("cleanup", function (t) {
+  rimraf.sync(fixtures)
   t.end()
 })
